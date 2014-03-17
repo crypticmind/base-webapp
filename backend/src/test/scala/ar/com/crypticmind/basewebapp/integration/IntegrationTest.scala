d24391a31eff7feede049c7223e0f1b765ff0d89
Carlos Ferreyra <cferreyra@despegar.com>
2014-03-17 16:33:16 -0300

    Changed base integration test to trait

--- backend/src/test/scala/ar/com/crypticmind/basewebapp/integration/IntegrationTest.scala
+++ backend/src/test/scala/ar/com/crypticmind/basewebapp/integration/IntegrationTest.scala
@@ -3,7 +3,7 @@ package ar.com.crypticmind.basewebapp.integration
 import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
 import ar.com.crypticmind.basewebapp.testsupport.TestCore
 
-class IntegrationTest extends TestCore with WordSpecLike with Matchers with BeforeAndAfterAll {
+trait IntegrationTest extends TestCore with WordSpecLike with Matchers with BeforeAndAfterAll {
 
   override def afterAll() {
     system.shutdown()
