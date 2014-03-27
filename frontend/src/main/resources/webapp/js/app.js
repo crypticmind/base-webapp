eb547d6977df5cbf11b53154132320a1a9d5fbf6
Carlos Ferreyra <cferreyra@despegar.com>
2014-03-27 18:33:42 -0300

    Base setup

---
+++ frontend/src/main/resources/webapp/js/app.js
@@ -0,0 +1,23 @@
+
+var app = angular.module('app', [
+    'ngRoute',
+    'appControllers'
+]);
+
+app.config([
+   '$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+            .when('/users', {
+                templateUrl: 'partials/user-list.html',
+                controller: 'UserListController'
+            })
+            .when('/users/:userId', {
+                templateUrl: 'partials/user-detail.html',
+                controller: 'UserDetailController'
+            })
+            .otherwise({
+                redirectTo: '/users'
+            });
+    }
+]);
---
+++ frontend/src/main/resources/webapp/js/controllers.js
@@ -0,0 +1,21 @@
+
+var appControllers = angular.module('appControllers', []);
+
+
+appControllers.controller('UserListController', [ '$scope', '$http',
+    function($scope, $http) {
+        $http.get('users.json').success(function(response) {
+            $scope.users = response.data;
+        });
+    }]);
+
+appControllers.controller('UserDetailController', [ '$scope', '$routeParams', '$http',
+    function($scope, $routeParams, $http) {
+        $http.get('user_' + $routeParams.userId + '.json').success(function(response) {
+            $scope.user = response.data;
+        });
+        $scope.user = {
+            id: 'unknown',
+            username: 'unknown'
+        }
+    }]);
